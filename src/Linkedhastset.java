988859487f60be519551a3ebf93eb7543a4c5d45
nehadubey751 <nehadubey751>
2023-07-26 18:11:04 +0530

    'collection'

---
+++ src/Linkedhastset.java
@@ -0,0 +1,18 @@
+import java.util.*;  
+public class Linkedhastset{  
+public static void main(String args[]){  
+LinkedHashSet<String> set=new LinkedHashSet<String>();  
+set.add("Ravi");  
+set.add("Vijay");  
+set.add("Ravi");  
+set.add("Ajay");  
+Iterator<String> itr=set.iterator();  
+while(itr.hasNext()){  
+System.out.println(itr.next());  
+}  
+}  
+}  
+
+
+
+	
